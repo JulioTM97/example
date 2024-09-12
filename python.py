f13115a8e6a23f7d9286d3285de0392283f1f96c
JULIO CESAR TAPIA MOYA <julio.tapia@idoppril.gob.do>
2024-09-12 10:12:44 -0400

    initial commit 3

--- python.py
+++ python.py
@@ -1,23 +1,24 @@
 import os
 import hashlib
+
 def save_password(password):
- # Vulnerabilidad: Uso de un algoritmo de hashing débil (MD5)
+    # Vulnerabilidad: Uso de un algoritmo de hashing débil (MD5)
     hashed_password = hashlib.md5(password.encode()).hexdigest()
     with open('passwords.txt', 'a') as f:
         f.write(hashed_password + '\n')
 
 def get_file(filename):
-# Vulnerabilidad: No se valida la entrada del usuario
+    # Vulnerabilidad: No se valida la entrada del usuario
     if os.path.exists(filename):
-    with open(filename, 'r') as f:
-        return f.read()
+        with open(filename, 'r') as f:
+            return f.read()
     else:
-    return "El archivo no existe"
+        return "El archivo no existe"
 
 def login(password):
-# Vulnerabilidad: Comparación de cadenas insegura
+    # Vulnerabilidad: Comparación de cadenas insegura
     with open('passwords.txt', 'r') as f:
-    for line in f:
-    if password == line.strip():
-        return True
+        for line in f:
+            if password == line.strip():
+                return True
     return False
